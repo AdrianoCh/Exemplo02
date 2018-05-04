b2f6e13c708a4b09003381e1d1c47e738b126eac
Adriano <adrianochaves93@gmail.com>
2018-05-04 14:40:32 -0300

    inserindo projeto

---
+++ exemplo02/src/test/java/pucrs/s2b/exemplo02/AppTest.java
@@ -0,0 +1,19 @@
+package pucrs.s2b.exemplo02;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+	
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
