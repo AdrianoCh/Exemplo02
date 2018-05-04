a803dbc608d9d46aed8e9f4a08db013a26465d53
Adriano <adrianochaves93@gmail.com>
2018-05-04 14:49:02 -0300

    adicionado metodo soma

--- exemplo02/src/main/java/pucrs/s2b/exemplo02/App.java
+++ exemplo02/src/main/java/pucrs/s2b/exemplo02/App.java
@@ -1,13 +1,22 @@
 package pucrs.s2b.exemplo02;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		int a= 20;
+		int b= 30;
+		int r = somar(a, b);
+		
+		if(r == 50) {
+			System.out.println("Resultado está certo");
+		} else {
+			System.out.println("O resultado está errado");
+		}
+	}
+
+	public static int somar(int a, int b) {
+		int r= a+b;
+		return r;
     }
 }
